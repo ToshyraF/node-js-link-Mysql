2fa1b7e57a5faee9c7952b65ff3059d2fcd2612b
ToshyraF <faryhost@gmail.com>
2017-03-18 12:05:56 +0700

    Add function insertData
    add data to table

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
   password : '1234',
-  database : 'world'
+  database : 'school'
 });
+function selectAll(){
 
-connection.connect();
-
-connection.query('SELECT * from city', function(err, rows, fields) {
+connection.query('SELECT * from student', function(err, rows, fields) {
   if (err) throw err;
 
   console.log('The solution is: ', rows);
 });
 
+}
+function insertData(){
+    connection.query('INSERT student values ("0987654321","book","gok@gmail.com");', function(err,result, fields) {
+    if (err) throw err;
+
+        console.log('success');
+        });
+}
+connection.connect();
+insertData();
+selectAll();
 connection.end();
