317eb918ecdb3198e30462ac13c4c403a3f842f6
ToshyraF <faryhost@gmail.com>
2017-03-19 22:36:30 +0700

    add front end
    write html show web basic for test .

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var express    = require('express');
+var app        = express();
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -5,14 +7,16 @@ var connection = mysql.createConnection({
   password : '1234',
   database : 'school'
 });
+app.use(express.static(__dirname+"/view" ));
+app.get('/',function(req,res){
+    res.render('index');
+});
 function selectAll(){
-
 connection.query('SELECT * from student', function(err, rows, fields) {
   if (err) throw err;
 
   console.log('The solution is: ', rows);
 });
-
 }
 function insertData(){
     connection.query('INSERT student values ("0987654321","book","gok@gmail.com");', function(err,result, fields) {
@@ -21,7 +25,7 @@ function insertData(){
         console.log('success');
         });
 }
-connection.connect();
-insertData();
-selectAll();
-connection.end();
+// connection.connect();
+// insertData();
+//connection.end();
+app.listen(3000);
